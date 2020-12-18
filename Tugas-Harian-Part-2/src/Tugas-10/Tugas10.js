666a7bdac23dfae1176d4f927ad5ce5c5a7f936e
khalidrinaldy <alid.rinaldy@gmail.com>
2020-12-18 12:08:14 +0700

    update Tugas-10

--- Tugas-Harian-Part-2/src/Tugas-10/Tugas10.js
+++ Tugas-Harian-Part-2/src/Tugas-10/Tugas10.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react'
 
-let dataHargaBuah = [
-    {nama: "Semangka", harga: 10000, berat: 1000},
-    {nama: "Anggur", harga: 40000, berat: 500},
-    {nama: "Strawberry", harga: 30000, berat: 400},
-    {nama: "Jeruk", harga: 30000, berat: 1000},
-    {nama: "Mangga", harga: 30000, berat: 500}
-];
-
 export default class Tugas10 extends Component {
     render() {
         return (
@@ -19,7 +11,7 @@ export default class Tugas10 extends Component {
                         <th>Harga</th>
                         <th>Berat</th>
                     </tr>
-                    {dataHargaBuah.map((item) => 
+                    {this.props.dataHargaBuah.map((item) => 
                         <tr className="table-row-child">
                             <td>{item.nama}</td>
                             <td>{item.harga}</td>
